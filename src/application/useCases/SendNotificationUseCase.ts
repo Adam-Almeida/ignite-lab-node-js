abd4f310131c4c35f6ccce879d9e93aaedc3bd4e
adam-almeida <adamportifolio@gmail.com>
2022-12-13 22:25:25 -0300

    feat: create send notification use case

---
+++ src/application/useCases/SendNotificationUseCase.ts
@@ -0,0 +1,30 @@
+import { Content } from '../entities/content';
+import { Notification } from '../entities/notification';
+
+interface ISendNotificationRequest {
+  recipientId: string;
+  content: string;
+  category: string;
+}
+
+interface ISendNotificationResponse {
+  notification: Notification;
+}
+
+export class SendNotificationUseCase {
+  async execute(
+    req: ISendNotificationRequest,
+  ): Promise<ISendNotificationResponse> {
+    const { recipientId, content, category } = req;
+
+    const notification = new Notification({
+      recipientId,
+      content: new Content(content),
+      category,
+    });
+
+    return {
+      notification,
+    };
+  }
+}
