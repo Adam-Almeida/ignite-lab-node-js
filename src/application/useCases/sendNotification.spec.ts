1409a4c40330736034a4f33e28dab5628311c32e
adam-almeida <adamportifolio@gmail.com>
2022-12-13 23:29:30 -0300

    test: create in memory repository test

--- src/application/useCases/sendNotification.spec.ts
+++ src/application/useCases/sendNotification.spec.ts
@@ -1,28 +1,22 @@
-import { Notification } from '../entities/notification';
+import { InMemoryNotificationsRepository } from '../../../test/repositories/inMemoryNotificationsRepository';
 import { SendNotificationUseCase } from './SendNotificationUseCase';
-
-const notifications: Notification[] = [];
-
-const notificationsRepository = {
-  async create(notification: Notification) {
-    notifications.push(notification);
-  },
-};
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
     const sendNotification = new SendNotificationUseCase(
       notificationsRepository,
     );
 
-    await sendNotification.execute({
+    const { notification } = await sendNotification.execute({
       recipientId: 'example-recipient-id',
       category: 'social',
       content: 'This is a notification',
     });
 
-    console.log(notifications);
+    // console.log(notifications);
 
-    expect(notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications[0]).toEqual(notification);
   });
 });
