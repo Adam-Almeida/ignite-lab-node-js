5e997c6e2f8b1a874fe98d0bf6e847d8753d1da1
adam-almeida <adamportifolio@gmail.com>
2022-12-14 21:38:45 -0300

    test: create test non existing notification

--- src/application/useCases/cancelNotification.spec.ts
+++ src/application/useCases/cancelNotification.spec.ts
@@ -2,6 +2,7 @@ import { Content } from '@application/entities/content';
 import { Notification } from '@application/entities/notification';
 import { InMemoryNotificationsRepository } from '@test/repositories/inMemoryNotificationsRepository';
 import { CancelNotificationUseCase } from './cancelNotificationUseCase';
+import { NotificationNotFoundError } from './errors/NotificationNotFoundError';
 
 describe('Cancelnotification', () => {
   it('should be able to cancel a notification', async () => {
@@ -25,5 +26,17 @@ describe('Cancelnotification', () => {
     expect(notificationsRepository.notifications[0].canceledAt).toEqual(
       expect.any(Date),
     );
+  });
+
+  it('should not be able to cancel a non exisiting notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotificationUseCase(
+      notificationsRepository,
+    );
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFoundError);
   });
 });
