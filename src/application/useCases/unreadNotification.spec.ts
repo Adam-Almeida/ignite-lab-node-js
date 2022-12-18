ef196dbc9ffaa00b90a0d40b6b824f8e35cfd5aa
adam-almeida <adamportifolio@gmail.com>
2022-12-18 12:19:17 -0300

    test: create unread notification test

---
+++ src/application/useCases/unreadNotification.spec.ts
@@ -0,0 +1,37 @@
+import { makeNotification } from '@test/factories/NotificationFactory';
+import { InMemoryNotificationsRepository } from '@test/repositories/inMemoryNotificationsRepository';
+import { NotificationNotFoundError } from './errors/NotificationNotFoundError';
+import { UnreadNotificationUseCase } from './UnreadNotificationUseCase';
+
+describe('Unread notification', () => {
+  it('should be able to read a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const unreadNotification = new UnreadNotificationUseCase(
+      notificationsRepository,
+    );
+
+    const notification = makeNotification();
+
+    await notificationsRepository.create(notification);
+
+    await unreadNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationsRepository.notifications[0].readAt).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it('should not be able to read a non exisiting notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const unreadNotification = new UnreadNotificationUseCase(
+      notificationsRepository,
+    );
+    expect(() => {
+      return unreadNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFoundError);
+  });
+});
